48c48f314268c813d621bf76a88cd219f663d1ce
Michi <michael@klauser.biz>
2014-12-03 15:41:10 +0100

    detect openmp

--- tardis/setup_package.py
+++ tardis/setup_package.py
@@ -1,6 +1,11 @@
 #setting the right include
 from setuptools import Extension
 import numpy as np
+import os
+import sys
+import tempfile
+import shutil
+from distutils.ccompiler import new_compiler
 
 randomkit_files = ['tardis/randomkit/rk_isaac.c', 'tardis/randomkit/rk_mt.c',
                    'tardis/randomkit/rk_primitive.c',
@@ -12,5 +17,60 @@ def get_extensions():
                       randomkit_files,
                       include_dirs=['tardis/randomkit', np.get_include()],
                       extra_compile_args=['-fopenmp'],
-                      extra_link_args=['-lgomp']
+                      extra_link_args=['-lgomp'],
+                      pyrex_compile_time_env = {'OPENMP': True}
                       )]
+
+##OpenMP
+
+def hasfunction(cc, funcname, include=None, extra_postargs=None):
+    # From http://stackoverflow.com/questions/7018879/disabling-output-when-compiling-with-distutils
+    tmpdir = tempfile.mkdtemp(prefix='hasfunction-')
+    devnull = oldstderr = None
+    try:
+        try:
+            fname = os.path.join(tmpdir, 'funcname.c')
+            print(fname)
+            f = open(fname, 'w')
+            f.write('#include <stdio.h>\n')
+            f.write('#include <stdio.h>\n')
+            f.write('int main(void) {\n')
+            f.write('    %s();\n' % funcname)
+            f.write('}\n')
+            f.close()
+            f = open(fname, 'r')
+            f.read()
+            f.close()
+            # Redirect stderr to /dev/null to hide any error messages
+            # from the compiler.
+            # This will have to be changed if we ever have to check
+            # for a function on Windows.
+            devnull = open('/dev/null', 'w')
+            oldstderr = os.dup(sys.stderr.fileno())
+            os.dup2(devnull.fileno(), sys.stderr.fileno())
+            objects = cc.compile([fname], output_dir=tmpdir, extra_postargs=['-fopenmp'] )
+            cc.link_executable(objects, os.path.join(tmpdir, "a.out"), extra_postargs=['-lgomp'] )
+        except:
+            return False
+        return True
+    finally:
+        if oldstderr is not None:
+            os.dup2(oldstderr, sys.stderr.fileno())
+        if devnull is not None:
+            devnull.close()
+            #shutil.rmtree(tmpdir)
+
+
+def detect_openmp():
+    "Does this compiler support OpenMP parallelization?"
+    compiler = new_compiler()
+    print "Attempting to autodetect OpenMP support... ",
+    hasopenmp = hasfunction(compiler, 'omp_get_num_threads')
+    if not hasopenmp:
+        print('No openMP found')
+    else:
+        print("OpenMP found")
+    return hasopenmp
+
+
+has_openmp = detect_openmp()
