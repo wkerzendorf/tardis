8591422b811be5f743f9d4c313e8feb1218121bc
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-08-03 15:24:52 +0200

    some changes

--- tardis/plasma/properties/property_collections.py
+++ tardis/plasma/properties/property_collections.py
@@ -19,19 +19,27 @@ basic_inputs = PlasmaPropertyCollection([TRadiative, Abundance, Density,
     TimeExplosion, AtomicData, JBlues, DilutionFactor, LinkTRadTElectron,
     RadiationFieldCorrectionInput, NLTESpecies, PreviousBetaSobolevs,
     PreviousElectronDensities])
+
 basic_properties = PlasmaPropertyCollection([BetaRadiation,
     Levels, Lines, AtomicMass, LevelPopulation, PartitionFunction,
     GElectron, IonizationData, NumberDensity, LinesLowerLevelIndex,
     LinesUpperLevelIndex, TauSobolev, LevelNumberDensity, IonNumberDensity,
     StimulatedEmissionFactor, SelectedAtoms, PhiGeneral])
+
 lte_ionization_properties = PlasmaPropertyCollection([PhiSahaLTE])
+
 lte_excitation_properties = PlasmaPropertyCollection([LevelBoltzmannFactorLTE])
+
 macro_atom_properties = PlasmaPropertyCollection([BetaSobolev,
     TransitionProbabilities])
+
 nebular_ionization_properties = PlasmaPropertyCollection([PhiSahaNebular,
     ZetaData, ElectronTemperature, BetaElectron, RadiationFieldCorrection])
+
 dilute_lte_excitation_properties = PlasmaPropertyCollection([
     LevelBoltzmannFactorDiluteLTE])
+
 non_nlte_properties = PlasmaPropertyCollection([LevelBoltzmannFactorNoNLTE])
+
 nlte_properties = PlasmaPropertyCollection([
     LevelBoltzmannFactorNLTE, NLTEData, NLTESpecies, LTEJBlues])
