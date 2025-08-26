59f71be22392aa73a3f62b38a61465247f9be07f
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2025-08-26 16:57:05 -0400

    Refactor opacity state initialization and interaction handling

--- tardis/opacities/opacity_state_numba.py
+++ tardis/opacities/opacity_state_numba.py
@@ -1,4 +1,3 @@
-from enum import IntEnum
 
 import numpy as np
 from numba import float64, int64
@@ -10,7 +9,7 @@ from tardis.transport.montecarlo.configuration import montecarlo_globals
 C_SPEED_OF_LIGHT = const.c.to("cm/s").value
 
 
-opacity_state_spec = [
+opacity_state_numba_spec = [
     ("electron_density", float64[:]),
     ("t_electrons", float64[:]),
     ("line_list_nu", float64[:]),
@@ -36,8 +35,8 @@ opacity_state_spec = [
 ]
 
 
-@jitclass(opacity_state_spec)
-class OpacityState:
+@jitclass(opacity_state_numba_spec)
+class OpacityStateNumba:
     def __init__(
         self,
         electron_density,
@@ -122,7 +121,7 @@ class OpacityState:
             OpacityState : a shallow copy of the current instance
         """
         # NOTE: This currently will not work with continuum processes since it does not slice those arrays
-        return OpacityState(
+        return OpacityStateNumba(
             self.electron_density[i],
             self.t_electrons[i],
             self.line_list_nu,
@@ -148,7 +147,7 @@ class OpacityState:
         )
 
 
-def opacity_state_initialize(
+def opacity_state_numba_initialize(
     plasma,
     line_interaction_type,
     disable_line_scattering,
@@ -257,7 +256,7 @@ def opacity_state_initialize(
         photo_ion_activation_idx = np.zeros(0, dtype=np.int64)
         k_packet_idx = np.int64(-1)
 
-    return OpacityState(
+    return OpacityStateNumba(
         electron_densities,
         t_electrons,
         line_list_nu,
@@ -283,7 +282,3 @@ def opacity_state_initialize(
     )
 
 
-class LineInteractionType(IntEnum):
-    SCATTER = 0
-    DOWNBRANCH = 1
-    MACROATOM = 2
