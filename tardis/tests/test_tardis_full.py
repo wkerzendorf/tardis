142d682eb4b2f8f19751ebed36de6ff10d85e1ae
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2026-02-17 08:42:34 -0500

    Disable virtual packets by default for performance; enable explicitly in tests requiring vpacket tracking

--- tardis/tests/test_tardis_full.py
+++ tardis/tests/test_tardis_full.py
@@ -65,6 +65,9 @@ class TestTransportSimple:
             str(example_configuration_dir / "tardis_configv1_verysimple.yml")
         )
         config["atom_data"] = atomic_data_fname
+        # Explicitly enable vpackets for tests that need them
+        config["montecarlo"]["no_of_virtual_packets"] = 5
+        config["spectrum"]["virtual"]["virtual_packet_logging"] = True
 
         simulation = Simulation.from_config(config)
         simulation.run_convergence()
